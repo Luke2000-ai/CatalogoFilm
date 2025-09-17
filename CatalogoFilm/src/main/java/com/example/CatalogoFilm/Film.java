a2c2e4d5c532f5162227ee7dec8e4b36c0459ddf
Anderozzi Luca <landreozzi@itsmeccanicabruzzo.eu>
2025-09-17 21:02:31 +0200

    primo file Inviato

---
+++ CatalogoFilm/src/main/java/com/example/CatalogoFilm/Film.java
@@ -0,0 +1,9 @@
+package com.example.CatalogoFilm;
+
+public class Film{
+    private int Id;
+    private String titolo;
+    private String regista;
+    private int anno;
+    private String genere;
+}
